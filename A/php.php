e9fcd275b4da056a09d2d857cb8004fd94a273ee
GustavoCaua12 <Medeiros.luizhenrique@yahoo.com.br>
2019-06-12 11:37:33 -0300

    Commit

---
+++ A/php.php
@@ -0,0 +1,14 @@
+<html>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<fieldset>
+	<b id="A">Qual a melhor forma de procurar estágio?</b>
+<?php
+echo "<br><br>Diretamente nas empresas (local ou site) <b>20%</b><br>";
+echo "<br>Através de eventos e sites de recrutamentos e/ou voltados a universitários <b>20%</b><br>";
+echo "<br>Procurar a Central de Estágio de faculdade <b>20%</b><br>";
+echo "<br>Indicação de amigos <b>20%</b><br>";
+echo "<br>Criar um site com seu próprio currículo, para ampliar a divulgação de seus objetivos profissionais <b>20%</b><br><br>" ;
+?>
+</fieldset>
+</html>
---
+++ C/php.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="style2.css">
+<body>
+<form name="loljinha" method="post" action="index.html">
+	<center><fieldset id="A">
+		<legend><fieldset>Seu pedido</fieldset></legend>
+<?php
+echo "<center><img src='NS.jpg'/><br><br>";
+echo "Nintendo Switch Red'n Blue</center><br>";
+echo "<br><label>Dados Pessoais</label><br>";
+echo "Seu nome: ".$_POST['nome'];
+echo "<br>Seu email: ".$_POST['email'];
+echo "<br>Seu CPF: ".$_POST['cpf'];
+echo "<br><br><label>Cartão de Crédito</label><br>";
+echo "Número do cartão: ".$_POST['cartao'];
+echo "<br>Bandeira do cartão: ".$_POST['bandeira'];
+?>
+	<center><br><button type="submit">Voltar as compras</button></center>
+	</fieldset></center>
+</body>
+</html>
